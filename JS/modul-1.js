b4c812674a7c5c20ea1436207f84da2b455c6807
Kornilov <dp310895kdi@gmail.com>
2022-10-18 20:59:25 +0300

    1 day

---
+++ JS/modul-1.js
@@ -0,0 +1,26 @@
+const valera = "145";
+console.log(valera);
+const min = 0;
+const max = 14;
+let total = 0;
+for (let i = min; i < max; i++) {
+    console.log(i);
+    if (i % 2 === 0) {
+        console.log("Четное ", i);
+    }
+}
+const productName = 'Droid';
+const pricePerItem = 2000;
+console.log(productName);
+// 'Droid'
+
+console.log(pricePerItem);
+function add(a, b, c) {
+  let result = a + b + c;
+  console.log(`Addition result equals ${result}`);
+}
+  // Change code above this line
+
+add(15, 27, 10);
+add(10, 20, 30);
+add(5, 10, 15);
